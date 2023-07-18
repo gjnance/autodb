7310e66f2db05fe7496675a99ccbcc6544bf8bf1
Greg Nance <gjnance@gmail.com>
2023-07-18 14:53:31 -0700

    fix: fix terraform errors

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
+  value = azurerm_resource_group.example.id
 }
 output "virtual_network_id" {
-  value = azurerm_virtual_network.vnet.id
+  value = azurerm_virtual_network.example.id
 }
