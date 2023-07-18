393e6154a3ed80880eb7b68e0f6ae6d331d1fb68
Greg Nance <gjnance@gmail.com>
2023-07-18 15:42:53 -0700

    fix: correct address_prefixes

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.0.1/24"]
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
