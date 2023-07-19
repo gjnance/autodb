153540e49c8b1dcdcedd6f37c77db62f25475e54
Greg Nance <gjnance@gmail.com>
2023-07-19 12:41:10 -0700

    chore: re-organize code

--- vm-resources/adb-setup.sh
+++ vm-resources/adb-setup.sh
@@ -1,7 +1,23 @@
-#!/bin/bash -e
-set -ex
+#!/bin/bash -ex
+# Logs to /var/log/cloud-init-output.log
 
+NGINX_ROOT=/var/www
+APP_DIR=$(NGINX_ROOT)/autodb
+
+echo "AutoDB: Executing $(basename "$0") as $USER"
+
+echo "AutoDB: Installing required packages"
 sudo apt-get update -y
 sudo apt-get install -y nginx php8.1-fpm php-mysql
 
-echo "AutoDB cloud-init.yaml completed successfully" > /var/log/adb-cloud-init.log
+echo "AutoDB: Cloning git repository under $HOME"
+cd $HOME && git clone https://github.com/gjnance/autodb.git
+
+echo "AutoDB: Installing application files"
+sudo mkdir $APP_DIR
+sudo cp -Rp $HOME/autodb/src/www/ $APP_DIR
+
+echo "AutoDB: Configuring NGINX"
+#TODO: Copy adb.conf 
+
+echo "AutoDB: cloud-init.yaml completed successfully" > /var/log/adb-cloud-init.log
