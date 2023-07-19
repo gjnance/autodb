c2c3e80748f581b8563f6e97267962ef7fa5581e
Greg Nance <gjnance@gmail.com>
2023-07-19 14:11:34 -0700

    fix: fix path and remove sudo (runs as root)

--- vm-resources/adb-setup.sh
+++ vm-resources/adb-setup.sh
@@ -9,21 +9,21 @@ echo "AutoDB: Script environment"
 env
 
 echo "AutoDB: Installing required packages"
-sudo apt-get update -y
-sudo apt-get install -y nginx php8.1-fpm php-mysql
+apt-get update -y
+apt-get install -y nginx php8.1-fpm php-mysql
 
 echo "AutoDB: Cloning git repository under ~"
 git clone -b deploy-via-terraform https://github.com/gjnance/autodb.git ~/autodb
 
 echo "AutoDB: Installing application files"
-sudo mkdir $APP_DIR
-sudo cp -Rp ~/autodb/src/www $APP_DIR/
+mkdir $APP_DIR
+cp -Rp ~/autodb/src/www/* $APP_DIR
 
 echo "AutoDB: Configuring NGINX"
-sudo cp ~/autodb/src/nginx/autodb_nginx.conf /etc/nginx/sites-available/autodb
+cp ~/autodb/src/nginx/autodb_nginx.conf /etc/nginx/sites-available/autodb
 
-sudo ln -s /etc/nginx/sites-available/autodb /etc/nginx/sites-enabled/
-sudo unlink /etc/nginx/sites-enabled/default
-sudo systemctl reload nginx
+ln -s /etc/nginx/sites-available/autodb /etc/nginx/sites-enabled/
+unlink /etc/nginx/sites-enabled/default
+systemctl reload nginx
 
 echo "AutoDB: cloud-init.yaml completed successfully" > /var/log/adb-cloud-init.log
