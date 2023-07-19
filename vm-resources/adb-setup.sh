42691e095845cc94507be55fac6a58c8f1ded4ef
Greg Nance <gjnance@gmail.com>
2023-07-19 13:59:35 -0700

    fix: Remove $HOME (not in env)

--- vm-resources/adb-setup.sh
+++ vm-resources/adb-setup.sh
@@ -12,15 +12,15 @@ echo "AutoDB: Installing required packages"
 sudo apt-get update -y
 sudo apt-get install -y nginx php8.1-fpm php-mysql
 
-echo "AutoDB: Cloning git repository under $HOME"
-cd $HOME && git clone -b deploy-via-terraform https://github.com/gjnance/autodb.git
+echo "AutoDB: Cloning git repository under ~"
+git clone -b deploy-via-terraform https://github.com/gjnance/autodb.git ~/autodb
 
 echo "AutoDB: Installing application files"
 sudo mkdir $APP_DIR
-sudo cp -Rp $HOME/autodb/src/www $APP_DIR/
+sudo cp -Rp ~/autodb/src/www $APP_DIR/
 
 echo "AutoDB: Configuring NGINX"
-sudo cp $HOME/autodb/src/nginx/autodb_nginx.conf /etc/nginx/sites-available/autodb
+sudo cp ~/autodb/src/nginx/autodb_nginx.conf /etc/nginx/sites-available/autodb
 
 sudo ln -s /etc/nginx/sites-available/autodb /etc/nginx/sites-enabled/
 sudo unlink /etc/nginx/sites-enabled/default
