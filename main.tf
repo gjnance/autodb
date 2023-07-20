e8c8a7b9da538c343909cca0fdbb03f3d8e1fe96
Greg Nance <gjnance@gmail.com>
2023-07-19 18:16:18 -0700

    fix: revert

--- main.tf
+++ main.tf
@@ -175,7 +175,7 @@ resource "azurerm_network_interface_security_group_association" "autodb" {
 resource "azurerm_mysql_flexible_server" "autodb" {
   name                   = "autodb-mysql-server"
   resource_group_name    = azurerm_resource_group.autodb.name
-  location               = "eastus2"
+  location               = azurerm_resource_group.autodb.location
   administrator_login    = var.mysql_administrator_login
   administrator_password = var.mysql_administrator_login_password
   backup_retention_days  = 7
